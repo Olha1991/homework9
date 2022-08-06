fdc8a0ab85ab843dedbee2b9e6413edf8efa1195
Homework1 <danisholha760@gmail.com>
2022-08-06 00:06:15 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        // Carlo Collodi Pinocchio 1881
+        // Stephen King Misery 1987
+
+        Author author1 = new Author( "Carlo", "Collodi");
+        Book book1 = new Book("Pinocchio", 1881, author1);
+        Author author2 = new Author("Stephen", "King");
+        Book book2 = new Book("Misery", 1987, author2);
+
+        System.out.println("Название: " + book1.getName() + " Год: " + book1.getYear() + " Автор: " + book1.getAuthor());
+        System.out.println("Название: " + book2.getName() + " Год: " + book2.getYear() + " Автор: " + book2.getAuthor());
+
+        book1.setYear(1883);
+        book2.setYear(1995);
+        System.out.println("Сеттер - " + book1.getYear());
+        System.out.println("Сеттер - " + book2.getYear());
     }
 }
